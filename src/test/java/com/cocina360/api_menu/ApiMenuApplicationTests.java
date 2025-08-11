f9b95f39f9cf1ce1823511ac52297dc8e04bd03e
Dska <ragd1260@hotmail.com>
2025-08-10 23:46:09 -0600

    configuracion basica en el archivo properties y estructura basica del proyecto

---
+++ src/test/java/com/cocina360/api_menu/ApiMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocina360.api_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
