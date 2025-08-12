c71783ff0fd7650b43e3f4747e6ae89c0092bddc
Dska <ragd1260@hotmail.com>
2025-08-12 15:38:04 -0600

    se crea el service y el CRUD

---
+++ src/main/java/com/cocina360/api_menu/service/PlatillosService.java
@@ -0,0 +1,29 @@
+package com.cocina360.api_menu.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cocina360.api_menu.model.Platillos;
+import com.cocina360.api_menu.repository.PlatillosRepo;
+
+@Service
+public class PlatillosService {
+
+    @Autowired
+    private PlatillosRepo platillosRepo;
+
+    public List<Platillos> listarPlatillos(){
+        return platillosRepo.findAll();
+    }
+
+    public Platillos nuevoPlatillo(Platillos platillo){
+        return platillosRepo.save(platillo);
+    }
+
+    public void eliminar(Long id){
+        this.platillosRepo.deleteById(id);
+    }
+    
+}
